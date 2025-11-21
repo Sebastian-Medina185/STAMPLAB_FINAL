76924c8c5ed311c58238a8e9790e6dcd7b71c153
Sebastian-Medina185 <sebastianmedinaechavarria@gmail.com>
2025-11-21 18:56:03 -0500

    Ultima version

--- models/insumo.js
+++ models/insumo.js
@@ -4,13 +4,6 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Insumo extends Model {
     static associate(models) {
-      // Relación muchos a muchos con Productos
-      Insumo.belongsToMany(models.Producto, {
-        through: models.ProductoInsumo,
-        foreignKey: 'InsumoID',
-        otherKey: 'ProductoID',
-        as: 'productos'
-      });
 
       // Relación con DetalleCompra
       Insumo.hasMany(models.DetalleCompra, {
@@ -26,40 +19,40 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
 
-Insumo.init({
-  InsumoID: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  Nombre: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  Stock: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
- Tipo: {
-  type: DataTypes.STRING(50),
-  allowNull: false,
-  defaultValue: 'Otro'
-},
-  PrecioTela: {               // campo existente (se mantiene)
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: true,
-    defaultValue: 0
-  },
-  Estado: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  }
-}, {
-  sequelize,
-  modelName: 'Insumo',
-  tableName: 'insumos',
-  timestamps: false
-});
+  Insumo.init({
+    InsumoID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    Nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    Tipo: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: 'Otro'
+    },
+    PrecioTela: {               // campo existente (se mantiene)
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true,
+      defaultValue: 0
+    },
+    Estado: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Insumo',
+    tableName: 'insumos',
+    timestamps: false
+  });
 
 
   return Insumo;
